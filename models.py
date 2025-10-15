f5adaad53d1644967488f1cc35c2862d83548b30
Faemex <fabrizzio.zayas05@gmail.com>
2025-10-15 13:49:49 -0300

    fix: entradas

--- models.py
+++ models.py
@@ -15,9 +15,13 @@ class Equipos:
         self.grupo = grupo
 
 class partido:
-    def __init__(self, fecha, hora_de_inicio, identificador_equipo_uno, identificador_equipo_dos, goles_equipo_uno , goles_equipo_dos, tarjetas_amarillas_equipo_uno, tarjetas_amarillas_equipo_dos, tarjetas_rojas_equipo_uno, tarjetas_rojas_equipo_dos, id_partido, puntos_equipo_uno, puntos_equipo_dos):
-        self.fecha = fecha
+    def __init__(self,anio , mes, dia, minuto, hora_de_inicio, identificador_equipo_uno, identificador_equipo_dos, goles_equipo_uno , goles_equipo_dos, tarjetas_amarillas_equipo_uno, tarjetas_amarillas_equipo_dos, tarjetas_rojas_equipo_uno, tarjetas_rojas_equipo_dos, id_partido, puntos_equipo_uno, puntos_equipo_dos, jornada):
+        self.amio = anio
+        self.mes = mes
+        self.dia = dia
+        self.minuto = minuto
         self.hora_de_inicio = hora_de_inicio
+        self.fecha = datetime(anio, mes, dia, hora_de_inicio, minuto)
         self.identificador_equipo_uno = identificador_equipo_uno
         self.identificador_equipo_dos = identificador_equipo_dos
         self.goles_equipo_uno = goles_equipo_uno
@@ -29,31 +33,9 @@ class partido:
         self.id_partido = id_partido
         self.puntos_equipo_uno = puntos_equipo_uno
         self.puntos_equipo_dos = puntos_equipo_dos
-
-class jornada_uno:
-    def __init__(self, fecha, identificador_de_partidos, partidos, numero):
-        self.fecha = fecha 
-        self.idnetificador_de_partidos = identificador_de_partidos
-        self.partidos = []
-    def agregar_partidos (self, partido):
-        self.partidos.append(partido)
-
-
-class jornada_dos:
-    def __init__(self, fecha, identificador_de_partidos, partidos):
-        self.fecha = fecha 
-        self.idnetificador_de_partidos = identificador_de_partidos
-        self.partidos = []
-    def agregar_partidos (self, partido):
-        self.partidos.append(partido)
-
-class jornada_tres:
-    def __init__(self, fecha, identificador_de_partidos, partidos):
-        self.fecha = fecha 
-        self.idnetificador_de_partidos = identificador_de_partidos
-        self.partidos = []
-    def agregar_partidos (self, partido):
-        self.partidos.append(partido)        
+        self.jornada = jornada
+        
+      
 
         
         
