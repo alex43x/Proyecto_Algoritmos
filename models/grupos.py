e2f0aa237b0c858c11e6f09f838e9300a5961cb4
Faemex <fabrizzio.zayas05@gmail.com>
2025-11-03 19:13:15 -0300

    feat: se agrega guardado de grupo

--- models/grupos.py
+++ models/grupos.py
@@ -1,8 +1,14 @@
+from database import insert_grupo
 class Grupos:
     def __init__(self, idGrupo, nombreGrupo):
         self.idGrupo = idGrupo
         self.nombreGrupo = nombreGrupo
-
+    def guardarG(self):
+            insert_grupo(
+                self.idGrupo,
+                self.nombreGrupo
+            )
+            print(f"Grupo {self.nombreGrupo} Guardado correctamente en la base de datos")
     def mostrarTabla(self, listaEquipos):
         # Muestra la tabla de posiciones del grupo
         equiposGrupo = [k for k in listaEquipos if k.idGrupo == self.idGrupo]
